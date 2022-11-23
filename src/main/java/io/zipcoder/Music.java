aa78fc1f97b65a0b92c780b50dc53b98773f2451
Freddy Mintarja <115741236+fmintar1@users.noreply.github.com>
2022-11-22 20:41:21 -0500

    Done

--- src/main/java/io/zipcoder/Music.java
+++ src/main/java/io/zipcoder/Music.java
@@ -9,6 +9,44 @@ public class Music {
     }
 
     public Integer selection(Integer startIndex, String selection){
-        return null;
+        if(playList[startIndex] == selection) {
+            return 0;
+        }
+        int counterForward = counterForward(startIndex, selection);
+        int counterBackward = counterBackward(startIndex, selection);
+        if(counterForward < counterBackward) {
+            return counterForward;
+        }
+        return counterBackward;
+    }
+
+    public int counterForward(Integer startIndex, String selection) {
+        int counterForward = 0;
+        int i = startIndex;
+        while(playList[i] != selection) {
+            if(playList[i] == selection) {
+                break;
+            }
+            i++;
+            counterForward++;
+            i = i % playList.length;
+        }
+        return counterForward;
+    }
+
+    public int counterBackward(Integer startIndex, String selection) {
+        int counterBackward = 0;
+        int i = startIndex;
+        while(playList[i] != selection) {
+            if(playList[i] == selection) {
+                break;
+            }
+            i--;
+            counterBackward++;
+            if(i < 0) {
+                i = (playList.length-1);
+            }
+        }
+        return counterBackward;
     }
 }
